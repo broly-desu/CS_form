21475b13417b680e190ad947a85ca6f58139dd7f
achills deed <js7201028@icloud.com>
2024-11-06 10:37:22 +0900

    コンストラクタとかの作成

--- CS_FORM/CS_FORM/Form1.cs
+++ CS_FORM/CS_FORM/Form1.cs
@@ -22,10 +22,12 @@ namespace CS_FORM
                 TestButton testButton = new TestButton(countcreate % 3 * 100, countcreate / 3 * 100, 100, 100,countstring);
                 Controls.Add(testButton);
             }
-            Label testlabel = new Label();
-            testlabel.Location = new Point(1000,0);
-            testlabel.Text = "aa";
+            testlabel testlabel = new testlabel("ラベルのテスト", 1000, 300, 100, 500);
             Controls.Add(testlabel);
-        }
+            ///Label testlabel = new Label();
+            ///testlabel.Location = new Point(1000,0);
+            ///testlabel.Text = "aa";
+            ///Controls.Add(testlabel);
+        }   
     }
 }
--- CS_FORM/CS_FORM/testlabel.cs
+++ CS_FORM/CS_FORM/testlabel.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CS_FORM
 {
-    internal class testlabel
+    internal class testlabel : Label
     {
+        public testlabel(string str,int x, int y,int width,int height)
+        {
+            Text = str;
+            Location = new Point(x, y);
+            Size = new Size(width,height);
+        }
     }
 }
